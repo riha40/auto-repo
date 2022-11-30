a122cb71c17c21b86a842d4533932303c88499f6
ku <buks48@gmail.com>
2022-11-30 20:32:45 +0000

    added comments to code

--- naked.py
+++ naked.py
@@ -10,7 +10,7 @@ print('Asteroid processing service')
 # Initiating and reading config values
 print('Loading configuration from file')
 
-# 
+#Pievieno NASA API key lai iegutu datus 
 nasa_api_key = "u9qTAtsZzMnqQObg76eckgPQMERle6hYia5nAV64"
 nasa_api_url = "https://api.nasa.gov/neo/"
 
@@ -19,31 +19,31 @@ dt = datetime.now()
 request_date = str(dt.year) + "-" + str(dt.month).zfill(2) + "-" + str(dt.day).zfill(2)  
 print("Generated today's date: " + str(request_date))
 
-
+#Izvada informaciju no NASA API 
 print("Request url: " + str(nasa_api_url + "rest/v1/feed?start_date=" + request_date + "&end_date=" + request_date + "&api_key=" + nasa_api_key))
 r = requests.get(nasa_api_url + "rest/v1/feed?start_date=" + request_date + "&end_date=" + request_date + "&api_key=" + nasa_api_key)
-
+#Izvada NASA API iegutos datus
 print("Response status code: " + str(r.status_code))
 print("Response headers: " + str(r.headers))
 print("Response content: " + str(r.text))
-
+#Parbauda vai status kods ir vienads ar 200
 if r.status_code == 200:
-
+#Panem datus no json faila
 	json_data = json.loads(r.text)
-
+#izveido tuksus masivus, kuros velak tiks ievietoti iegutie dati no NASA API
 	ast_safe = []
 	ast_hazardous = []
-
+#Cik ir ieguto elementu skaits
 	if 'element_count' in json_data:
 		ast_count = int(json_data['element_count'])
 		print("Asteroid count today: " + str(ast_count))
-
+#Parbauda vai sodien ir kads asteroids un vai ir japilda talaka koda dala
 		if ast_count > 0:
-			for val in json_data['near_earth_objects'][request_date]:
+			for val in json_data['near_earth_objects'][request_date]: #Iet cauri ciklam, kas skatas, datus par asteroidu
 				if 'name' and 'nasa_jpl_url' and 'estimated_diameter' and 'is_potentially_hazardous_asteroid' and 'close_approach_data' in val:
 					tmp_ast_name = val['name']
 					tmp_ast_nasa_jpl_url = val['nasa_jpl_url']
-					if 'kilometers' in val['estimated_diameter']:
+					if 'kilometers' in val['estimated_diameter']: #Ja ir km asterioidi, tad pieskir vertibas pie lielaka un mazaka
 						if 'estimated_diameter_min' and 'estimated_diameter_max' in val['estimated_diameter']['kilometers']:
 							tmp_ast_diam_min = round(val['estimated_diameter']['kilometers']['estimated_diameter_min'], 3)
 							tmp_ast_diam_max = round(val['estimated_diameter']['kilometers']['estimated_diameter_max'], 3)
@@ -54,35 +54,35 @@ if r.status_code == 200:
 						tmp_ast_diam_min = -1
 						tmp_ast_diam_max = -1
 
-					tmp_ast_hazardous = val['is_potentially_hazardous_asteroid']
+					tmp_ast_hazardous = val['is_potentially_hazardous_asteroid'] #Ja ir bistams, tad pieskir vertibu
 
-					if len(val['close_approach_data']) > 0:
+					if len(val['close_approach_data']) > 0: #Ja asteroids ir tuvu zemei, tad kada ir bijusi si distance un laiku, kad notika
 						if 'epoch_date_close_approach' and 'relative_velocity' and 'miss_distance' in val['close_approach_data'][0]:
 							tmp_ast_close_appr_ts = int(val['close_approach_data'][0]['epoch_date_close_approach']/1000)
 							tmp_ast_close_appr_dt_utc = datetime.utcfromtimestamp(tmp_ast_close_appr_ts).strftime('%Y-%m-%d %H:%M:%S')
 							tmp_ast_close_appr_dt = datetime.fromtimestamp(tmp_ast_close_appr_ts).strftime('%Y-%m-%d %H:%M:%S')
-
+# Kads ir bijis atrums sim asteroidam un cik tuvu ir bijis
 							if 'kilometers_per_hour' in val['close_approach_data'][0]['relative_velocity']:
 								tmp_ast_speed = int(float(val['close_approach_data'][0]['relative_velocity']['kilometers_per_hour']))
 							else:
 								tmp_ast_speed = -1
-
+#Ja ir bijis tuvu zemei, tad kads ir bijis sis attalums no zemes
 							if 'kilometers' in val['close_approach_data'][0]['miss_distance']:
 								tmp_ast_miss_dist = round(float(val['close_approach_data'][0]['miss_distance']['kilometers']), 3)
 							else:
 								tmp_ast_miss_dist = -1
-						else:
+						else: #Ja nav tadadu bijusi tad tiek pieskirtas vertibas
 							tmp_ast_close_appr_ts = -1
 							tmp_ast_close_appr_dt_utc = "1969-12-31 23:59:59"
 							tmp_ast_close_appr_dt = "1969-12-31 23:59:59"
-					else:
+					else: #Ja nav tadadu bijusi tad tiek pieskirtas vertibas
 						print("No close approach data in message")
 						tmp_ast_close_appr_ts = 0
 						tmp_ast_close_appr_dt_utc = "1970-01-01 00:00:00"
 						tmp_ast_close_appr_dt = "1970-01-01 00:00:00"
 						tmp_ast_speed = -1
 						tmp_ast_miss_dist = -1
-
+#Tiek izvaditi dati, kas tika ieguti no NASA API key
 					print("------------------------------------------------------- >>")
 					print("Asteroid name: " + str(tmp_ast_name) + " | INFO: " + str(tmp_ast_nasa_jpl_url) + " | Diameter: " + str(tmp_ast_diam_min) + " - " + str(tmp_ast_diam_max) + " km | Hazardous: " + str(tmp_ast_hazardous))
 					print("Close approach TS: " + str(tmp_ast_close_appr_ts) + " | Date/time UTC TZ: " + str(tmp_ast_close_appr_dt_utc) + " | Local TZ: " + str(tmp_ast_close_appr_dt))
@@ -94,17 +94,17 @@ if r.status_code == 200:
 					else:
 						ast_safe.append([tmp_ast_name, tmp_ast_nasa_jpl_url, tmp_ast_diam_min, tmp_ast_diam_max, tmp_ast_close_appr_ts, tmp_ast_close_appr_dt_utc, tmp_ast_close_appr_dt, tmp_ast_speed, tmp_ast_miss_dist])
 
-		else:
+		else: #Ja nebija neviens asteroids, tad tiek izvadits pazinojums un netiek pildits iepriekseja koda dala
 			print("No asteroids are going to hit earth today")
-
+#Cik daudz ir bijusu drosu un cik daudz ir nedrosu asteroidu bijis
 	print("Hazardous asteorids: " + str(len(ast_hazardous)) + " | Safe asteroids: " + str(len(ast_safe)))
 
-	if len(ast_hazardous) > 0:
+	if len(ast_hazardous) > 0: #Parbauda vai ir bijusi kadi bistami asteroidi
 
-		ast_hazardous.sort(key = lambda x: x[4], reverse=False)
+		ast_hazardous.sort(key = lambda x: x[4], reverse=False) #Karto iegutos datus
 
-		print("Today's possible apocalypse (asteroid impact on earth) times:")
-		for asteroid in ast_hazardous:
+		print("Today's possible apocalypse (asteroid impact on earth) times:") #Izvada cik daudz ir bijusu bistamu asteroidu zemes tuvuma
+		for asteroid in ast_hazardous: 
 			print(str(asteroid[6]) + " " + str(asteroid[0]) + " " + " | more info: " + str(asteroid[1]))
 
 		ast_hazardous.sort(key = lambda x: x[8], reverse=False)
@@ -112,5 +112,5 @@ if r.status_code == 200:
 	else:
 		print("No asteroids close passing earth today")
 
-else:
+else: #Nepieklust ievaditajam NASA API key
 	print("Unable to get response from API. Response code: " + str(r.status_code) + " | content: " + str(r.text))
